6f94a85ae6599715c1d5c5bd6896269e989544a9
Katerina Bosko <katerinabosko@gmail.com>
2021-11-22 22:41:30 -0800

    added scripts for queries and replacing old SQL foreign keys with MongoDB _id for meal_id in ratings and orders collections

--- GhostKitchen-II/db/migration_SQLtoMongo/2_initializeDB.sh
+++ GhostKitchen-II/db/migration_SQLtoMongo/2_initializeDB.sh
@@ -4,3 +4,8 @@ mongoimport -h localhost:27017 -d GhostKitchen -c orders --jsonArray --drop ./db
 mongoimport -h localhost:27017 -d GhostKitchen -c customers --jsonArray --drop ./db/migration_SQLtoMongo/Customers.json
 mongoimport -h localhost:27017 -d GhostKitchen -c meals --jsonArray --drop ./db/migration_SQLtoMongo/Meals.json
 mongoimport -h localhost:27017 -d GhostKitchen -c locations --jsonArray --drop ./db/migration_SQLtoMongo/Locations.json
+mongoimport -h localhost:27017 -d GhostKitchen -c ratings --jsonArray --drop ./db/migration_SQLtoMongo/Ratings.json
+
+./db/migration_SQLtoMongo/replace_meal_id.js
+
+mongodump --host="localhost:27017" --db="GhostKitchen" --gzip --out ./db/dump/
